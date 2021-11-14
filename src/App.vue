238fd7ee26f7ae674155226c7ae6d47b77cf51f6
wengzijia <1486612633@qq.com>
2021-11-14 20:04:12 +0800

    苏宁移动端适配

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,9 @@
         <van-tabbar-item to="/" icon="like-o">猜你喜欢</van-tabbar-item>
         <van-tabbar-item to="/sort" icon="search">分类</van-tabbar-item>
         <van-tabbar-item to="/top" icon="orders-o">排行榜</van-tabbar-item>
-        <van-tabbar-item to="/cart" icon="shopping-cart-o">购物车</van-tabbar-item>
+        <van-tabbar-item to="/cart" icon="shopping-cart-o"
+          >购物车</van-tabbar-item
+        >
         <van-tabbar-item to="/mytesco" icon="circle">我的易购</van-tabbar-item>
       </van-tabbar>
     </div>
@@ -14,20 +16,30 @@
 </template>
 
 <style lang="scss">
-html{
+html {
   padding-bottom: 2rem;
 }
-#nav{
+#nav {
   width: 100%;
   // border: 2px solid black;
-  position:fixed;
+  position: fixed;
   bottom: 0;
   display: flex;
   justify-content: center;
 }
 
- #box{
-    position:relative;
-    width: 15rem;
+#box {
+  box-sizing: content-box;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: 0 auto;
+  width: 15rem;
+  height: 2rem;
+  z-index: 199;
+  border-top: 1px solid #eee;
+  background: #fff;
+  transition: all 0.2s linear;
 }
 </style>
